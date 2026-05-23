1c0381daff0f60dfea7e8b066718c868720c975c
frenzzi <MrChiter3@yandex.ru>
2026-05-23 11:00:51 +0700

    Itog

--- obj/Debug/net8.0/IDZ_1_MVP_3.AssemblyInfo.cs
+++ obj/Debug/net8.0/IDZ_1_MVP_3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IDZ_1_MVP_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb00a6b6d1def3c06f60e853c578e0493aeb4dd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe8885dda111954ca59efb44abbf952be8cf9bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("IDZ_1_MVP_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IDZ_1_MVP_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
